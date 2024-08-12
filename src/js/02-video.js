1b49900940b9804df2b2c92cce83658b3a0dacd1
rebecalasconi <rebecavoicilas@yahoo.com>
2024-08-13 10:56:08 +1400

    revizuire 1

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,13 @@
+"use strict";
 
+import VimeoPlayer from "@vimeo/player";
+
+const onPlay = function() {
+    let currentTime = player.currentTime();
+    let startTime = 0;
+    startTime = currentTime;
+    console.log(startTime);
+    // data is an object containing properties specific to that event
+};
+
+player.on('play', onPlay);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,39 @@
+"use strict";
+import lodash from "lodash.throttle";
+
+let emailInput = document.querySelector(`input[name=email]`);
+let messageInput = document.querySelector(`textarea[name=message]`);
+const button = document.querySelector(`button[type=submit]`);
+
+emailInput.addEventListener("input", updateValue);
+messageInput.addEventListener("input", updateValue);
+let email =  emailInput.textContent;
+let message =  messageInput.textContent;
+
+
+ 
+function updateValue(e) {
+    if (e.target === emailInput ) {
+    email = e.currentTarget.value;
+ }  else  {
+    message = e.currentTarget.value;
+ }
+    let data = `${email}, ${message}`; 
+    localStorage.setItem("feedback-form-state", data);
+
+    throt_fun();
+};
+
+let throt_fun = _.throttle(updateValue(), 500);
+
+//
+function pageRefresh() {
+    let nameFromLocalStorage = localStorage.getItem("feedback-form-state");
+    let array = nameFromLocalStorage.split(",");
+    
+    if(localStorage) {
+    email.textContent = array[0];
+    message.textContent = array[1];
+    }
+};
 
