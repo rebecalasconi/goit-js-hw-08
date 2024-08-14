d5bf63380565f8c878cc4959affaa3549397f1a2
rebecalasconi <rebecavoicilas@yahoo.com>
2024-08-15 01:18:07 +1400

    update exercitul 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -5,32 +5,82 @@ import throttle from 'lodash.throttle';
 let emailInput = document.querySelector(`input[name=email]`);
 let messageInput = document.querySelector(`textarea[name=message]`);
 const button = document.querySelector(`button[type=submit]`);
+const STORAGE_KEY = 'feedback-form-state';
+const form = document.querySelector(".feedback-form");
 
-emailInput.addEventListener("input", updateValue);
-messageInput.addEventListener("input", updateValue);
-let email =  emailInput.textContent;
-let message =  messageInput.textContent;
-    
- document.addEventListener('input', _.throttle(handleThrottling, 60000));
- 
-function updateValue(e) {
-    if (e.target === emailInput ) {
-    email = e.currentTarget.value;
- }  else  {
-    message = e.currentTarget.value;
- }
-    let data = `${email}, ${message}`; 
-    localStorage.setItem("feedback-form-state", data);
-};
+const createText = (email, message) => ({
+    email,
+    message,
+  });
+  
+
+function save(key, value) {
+    try {
+      const data = JSON.stringify(value);
+      localStorage.setItem(key, data);
+    } catch (error) {}
+  }
+  
+  function load(key) {
+    try {
+      const data = localStorage.getItem(key);
+      return data === null ? undefined : JSON.parse(data);
+    } catch (error) {
+      console.error(error);
+    }
+  }
 
 
-window.addEventListener('DOMContentLoaded', pageRefresh);
+  emailInput.addEventListener("input", updateValue);
+  messageInput.addEventListener("input", updateValue);
+  
+  function updateValue(e) {
+  if (e.target === emailInput ) {
+  email = e.currentTarget.value
+  }  else  {
+  message = e.currentTarget.value;}
 
-function pageRefresh() {
-    let nameFromLocalStorage = localStorage.getItem("feedback-form-state");
-    let array = nameFromLocalStorage.split(" "); 
-    email = array[0];
-    message = array[1];
-    };
+  addTaskToLocalStorage(emailInput.value, messageInput.value);
+}
 
-pageRefresh();
+function addTaskToLocalStorage(emailInput, messageInput) {
+  const currentState = load(STORAGE_KEY);
+  const newTask = createText(emailInput, messageInput);
+  if (currentState === undefined) {
+    save(STORAGE_KEY, [newTask]);
+  } else {
+    currentState.push(newTask);
+    save(STORAGE_KEY, currentState);
+  }
+}
+
+window.addEventListener('DOMContentLoaded', fillTaskList);
+
+function fillTaskList() {
+  const currentState = load(STORAGE_KEY);
+  if (currentState !== undefined) {
+    currentState.forEach(task => {
+        const lastEl = currentState.slice(-1)[0]
+        emailInput.value = lastEl.email;
+        messageInput.value = lastEl.message;
+    });
+  }
+}
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+  
+    if (emailInput.value === "" || messageInput.value === "") {
+      alert("Trebuie sa introduci valori in ambele campuri!");
+    } else {
+      alert("Formularul a fost trimis!");
+      console.log(emailInput.value, messageInput.value);
+      emailInput.value = "";
+      messageInput.value = "";
+      localStorage.removeItem("feedback-form-state");
+
+    }
+  });
+
+  const throttled = _.throttle(updateValue, 600000, { 'trailing': false });
+  jQuery(form).on('input', throttled);
